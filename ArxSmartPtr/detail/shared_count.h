2871b6028cf6c9dab7714a50c2aaaaa7feaf535a
arljba <33658992+arljba@users.noreply.github.com>
2020-07-22 09:23:14 +0200

    Change for use with gcc instead of g++

--- ArxSmartPtr/detail/shared_count.h
+++ ArxSmartPtr/detail/shared_count.h
@@ -7,19 +7,29 @@
 
 // imported and simplified from https://github.com/boostorg/smart_ptr
 
+//Has to be added when compiling with gcc instead of g++
+void operator delete(void *ptr, size_t size)
+{
+    free(ptr);
+}
+
+void operator delete[](void *ptr, size_t size)
+{
+    free(ptr);
+}
+
 namespace arx
 {
     namespace detail
     {
         class sp_counted_base
         {
-            sp_counted_base(const sp_counted_base&) = delete;
-            sp_counted_base& operator= (const sp_counted_base&) = delete;
+            sp_counted_base(const sp_counted_base &) = delete;
+            sp_counted_base &operator=(const sp_counted_base &) = delete;
 
             int use_count_;
 
         public:
-
             sp_counted_base() : use_count_(1) {}
             virtual ~sp_counted_base() {}
 
@@ -42,20 +52,18 @@ namespace arx
             }
         };
 
-
         template <typename T>
         class sp_counted_impl : public sp_counted_base
         {
-            T* ptr;
+            T *ptr;
 
-            sp_counted_impl(const sp_counted_impl&) = delete;
-            sp_counted_impl& operator= (const sp_counted_impl&) = delete;
+            sp_counted_impl(const sp_counted_impl &) = delete;
+            sp_counted_impl &operator=(const sp_counted_impl &) = delete;
 
             typedef sp_counted_impl<T> this_type;
 
         public:
-
-            sp_counted_impl(T* ptr) : ptr(ptr) {}
+            sp_counted_impl(T *ptr) : ptr(ptr) {}
             virtual ~sp_counted_impl() {}
 
             virtual void dispose() override
@@ -64,30 +72,26 @@ namespace arx
             }
         };
 
-
-
         class shared_count
         {
         private:
-
-            sp_counted_base* pi_;
+            sp_counted_base *pi_;
 
         public:
-
             shared_count() : pi_(0)
             {
             }
 
-            explicit shared_count(sp_counted_base* p) : pi_(p)
+            explicit shared_count(sp_counted_base *p) : pi_(p)
             {
             }
 
-            template<class Y>
-            explicit shared_count(Y* p) : pi_(0)
+            template <class Y>
+            explicit shared_count(Y *p) : pi_(0)
             {
                 pi_ = new sp_counted_impl<Y>(p);
 
-                if(pi_ == 0)
+                if (pi_ == 0)
                 {
                     delete p;
                 }
@@ -95,34 +99,38 @@ namespace arx
 
             ~shared_count()
             {
-                if(pi_ != 0) pi_->release();
+                if (pi_ != 0)
+                    pi_->release();
             }
 
-            shared_count(const shared_count& r) : pi_(r.pi_)
+            shared_count(const shared_count &r) : pi_(r.pi_)
             {
-                if(pi_ != 0) pi_->add_ref();
+                if (pi_ != 0)
+                    pi_->add_ref();
             }
 
-            shared_count(shared_count&& r) : pi_(r.pi_)
+            shared_count(shared_count &&r) : pi_(r.pi_)
             {
                 r.pi_ = 0;
             }
 
-            shared_count& operator= (const shared_count& r)
+            shared_count &operator=(const shared_count &r)
             {
-                sp_counted_base* tmp = r.pi_;
-                if(tmp != pi_)
+                sp_counted_base *tmp = r.pi_;
+                if (tmp != pi_)
                 {
-                    if(tmp != 0) tmp->add_ref();
-                    if(pi_ != 0) pi_->release();
+                    if (tmp != 0)
+                        tmp->add_ref();
+                    if (pi_ != 0)
+                        pi_->release();
                     pi_ = tmp;
                 }
                 return *this;
             }
 
-            void swap(shared_count& r)
+            void swap(shared_count &r)
             {
-                sp_counted_base* tmp = r.pi_;
+                sp_counted_base *tmp = r.pi_;
                 r.pi_ = pi_;
                 pi_ = tmp;
             }
@@ -142,7 +150,7 @@ namespace arx
                 return pi_ == 0;
             }
 
-            friend inline bool operator== (const shared_count& a, const shared_count& b)
+            friend inline bool operator==(const shared_count &a, const shared_count &b)
             {
                 return a.pi_ == b.pi_;
             }
@@ -151,7 +159,5 @@ namespace arx
     } // namespace detail
 
 } // namespace arx
-
-
 
 #endif // ARX_SMART_PTR_DETAIL_SHARED_COUNT_H
